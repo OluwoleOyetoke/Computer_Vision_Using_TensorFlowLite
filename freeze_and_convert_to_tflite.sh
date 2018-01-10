48a60169b1518fe2fc5ef33143a90a8026e2d72c
Oluwole Oyetoke <oluwoleoyetoke@gmail.com>
2018-01-10 17:18:24 +0000

    transformation option for weight_round added to flite converter script

--- freeze_and_convert_to_tflite.sh
+++ freeze_and_convert_to_tflite.sh
@@ -30,14 +30,14 @@ bazel-bin/tensorflow/python/tools/freeze_graph --input_graph=$1 --input_checkpoi
 #bazel build /tensorflow/tools/graph_transforms:summarize_graph
 bazel-bin/tensorflow/tools/graph_transforms/summarize_graph --in_graph=$4
 
-#OPTIMIZE GRAPH
+#TRANSFORM/OPTIMIZE GRAPH
 #bazel build tensorflow/tools/graph_transforms:transform_graph
-bazel-bin/tensorflow/tools/graph_transforms/transform_graph --in_graph=$4 --out_graph=${10} --inputs=$5 --outputs=$6 --transforms='strip_unused_nodes(type=float, shape="1,227,227,3") remove_nodes(op=Identity, op=CheckNumerics) fold_old_batch_norms fold_batch_norms'
+bazel-bin/tensorflow/tools/graph_transforms/transform_graph --in_graph=$4 --out_graph=${10} --inputs=$5 --outputs=$6 --transforms='strip_unused_nodes(type=float, shape="1,227,227,3") remove_nodes(op=Identity, op=CheckNumerics) fold_constants(ignore_errors=true) round_weights(num_steps=256) obfuscate_names sort_by_execution_order fold_old_batch_norms fold_batch_norms'
 
 
 #CONVERT TO TFLITE MODEL
 #bazel build /tensorflow/contrib/lite/toco:toco
-bazel-bin/tensorflow/contrib/lite/toco/toco --input_format=TENSORFLOW_GRAPHDEF --input_file=${10} --output_format=TFLITE --output_file=$7 --inference_type=$9 --#input_type=$8 --input_arrays=$5 --output_arrays=$6 --inference_input_type=$8 --input_shapes=1,227,227,3
+bazel-bin/tensorflow/contrib/lite/toco/toco --input_format=TENSORFLOW_GRAPHDEF --input_file=${10} --output_format=TFLITE --output_file=$7 --inference_type=$9 --input_type=$8 --input_arrays=$5 --output_arrays=$6 --inference_input_type=$8 --input_shapes=1,227,227,3
 
 
 
